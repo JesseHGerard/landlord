2dad865a9657883de88a69e6fa76a5087a407b5f
Jesse Gerard <jessehgerard@gmail.com>
2017-12-15 17:31:48 -0500

    development server set to jaws

--- config/config.js
+++ config/config.js
@@ -1,9 +1,9 @@
 module.exports = {
 	"development": {
-		"username": "root",
-		"password": "",
-		"database": "roach_motel",
-		"host": "localhost",
+		"username": process.env.jaws_username,
+		"password": process.env.jaws_password,
+		"database": 'hpymvb5de1i66bl4',
+		"host": process.env.jaws_host,
 		"port": 3306,
 		"dialect": "mysql"
 	},
