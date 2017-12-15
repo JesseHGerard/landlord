459747fa3f0b7e29c9fce06a2fe98b1c33bf2675
Jesse Gerard <jessehgerard@gmail.com>
2017-12-15 18:12:21 -0500

    remove __dirname from config

--- models/index.js
+++ models/index.js
@@ -5,16 +5,16 @@ var path      = require('path');
 var Sequelize = require('sequelize');
 var basename  = path.basename(__filename);
 var env       = process.env.NODE_ENV || 'development';
-var config    = require(__dirname + '/../config/config.js')[env];
+var config    = require('/../config/config.js')[env];
 var db        = {};
 
-// if (config.use_env_variable) {
-// 	var sequelize = new Sequelize(process.env[config.use_env_variable], config);
-// } else {
-//
-// }
+if (config.use_env_variable) {
+	var sequelize = new Sequelize(process.env[config.use_env_variable], config);
+} else {
+	var sequelize = new Sequelize(config.database, config.username, config.password, config);
+}
 
-var sequelize = new Sequelize(config.database, config.username, config.password, config);
+
 
 fs
 	.readdirSync(__dirname)
