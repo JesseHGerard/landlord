c44b7bcb2cdb64b722ee0117433ca0373baeb158
Jesse Gerard <jessehgerard@gmail.com>
2017-12-15 19:11:54 -0500

    comment out production

--- config/config.js
+++ config/config.js
@@ -19,10 +19,10 @@ module.exports = {
 		"dialect": "mysql"
 	},
 	"production": {
-		"username": process.env.jaws_username,
-		"password": process.env.jaws_password,
-		"database": 'hpymvb5de1i66bl4',
-		"host": process.env.jaws_host,
+		// "username": process.env.jaws_username,
+		// "password": process.env.jaws_password,
+		// "database": 'hpymvb5de1i66bl4',
+		// "host": process.env.jaws_host,
 		"port": 3306,
 		"dialect": "mysql"
 	}
