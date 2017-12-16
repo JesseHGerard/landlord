8a6ef95123a7a07d2c93cacab182c02cbca0dd62
Jesse Gerard <jessehgerard@gmail.com>
2017-12-15 19:16:37 -0500

    add error log

--- server.js
+++ server.js
@@ -1,18 +1,14 @@
 var checkEnv = require('./config/env.js');
 
-
-
-
 // define variables that should remain secret in heroku as envrionmental variables
 // for local testing, also place in keys.js AND define as process.env in env.js
-
 
 const runServer = () => {
 	var express = require('express');
 	var exphbs = require('express-handlebars');
 	var bodyParser = require('body-parser');
 	var session = require('express-session');
-	//var passport = require('./config/passport');
+	var passport = require('./config/passport');
 	var db = require('./models');
 
 	var app = express();
@@ -21,9 +17,9 @@ const runServer = () => {
 	app.use(express.static("public"));
 	app.use(bodyParser.urlencoded({ extended: false }));
 	app.use(bodyParser.json());
-	//app.use(session({ secret: "r0ach m0t31", resave: true, saveUninitialized: true }));
-	//app.use(passport.initialize());
-	//app.use(passport.session());
+	app.use(session({ secret: "r0ach m0t31", resave: true, saveUninitialized: true }));
+	app.use(passport.initialize());
+	app.use(passport.session());
 	app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 	app.set("view engine", "handlebars");
 
@@ -44,4 +40,4 @@ const runServer = () => {
 checkEnv()
 .then(resolve => {
 	runServer();
-});
+}).catch(error => console.log(error));
