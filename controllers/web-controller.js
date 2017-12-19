6e2a36b69cb56d24503fd589c5e0436e99a965d1
the-realest-stu <stuartlindstrom@gmail.com>
2017-12-19 11:24:09 -0500

    landlords can't submit issues

--- controllers/web-controller.js
+++ controllers/web-controller.js
@@ -9,17 +9,17 @@ module.exports = function(app) {
   app.get("/", function(req, res) {
 	//if (req.user && req.user.userType === 'tenant') return res.redirect("/tenant/dash");
 	//if (req.user && req.user.userType === 'landlord') return res.redirect("/landlord/dash");
-	if (req.user && req.user.userType === 'tenant') {
-		db.Issue.findAll({
+	if (req.user && (req.user.userType === 'tenant' || req.user.userType === 'landlord')) {
+		/*db.Issue.findAll({
 			where: {
 				BuildingId: req.user.BuildingId
 			},
 			include: [db.Tenant, db.Building],
 			order: [['createdAt', 'DESC']],
 		}).then(issues => {
-			// console.log(JSON.stringify(issues, null, 2));
 			res.render("dashboard", {user: req.user, issues: issues});
-		});
+		});*/
+		res.render("dashboard", {user: req.user});
 	} else res.render("index");
   });
 
